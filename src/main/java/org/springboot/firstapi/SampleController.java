d4ea2143b755f22daf904c307caa72f83c3f620b
Nitesh Kumar <niteshkg23@gmail.com>
2024-05-05 22:23:07 +0530

    first api commit

---
+++ src/main/java/org/springboot/firstapi/SampleController.java
@@ -0,0 +1,27 @@
+package org.springboot.firstapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// This controller will be containing HTTP API's
+// domain of the application will be local host and port no is 8080
+// localhost : 8080/sample
+// All the API's for /sample endpoint should come to this controller
+// dispatcher servlet will take the request received on 8080/sample and pass it on to the controller
+
+@RequestMapping("/sample")
+public class SampleController {
+    // http://localhost:8080/sample/sayHello/Nitesh
+    @GetMapping("/sayHello/{name}")
+    public String sayHello(@PathVariable("name") String name){
+        return "Hi "+name;
+    }
+    // http://localhost:8080/sample/sayBye
+    @GetMapping("/sayBye")
+    public String sayBye(){
+        return "Bye World!";
+    }
+}
